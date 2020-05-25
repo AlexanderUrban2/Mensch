4dce15926d3ae7e2082bb8e188061cfb21b12132
Christoph-Mitutoyo <christoph.boehringer@mitutoyo-ctl.de>
2020-05-25 17:25:54 +0200

    move licences into ReadMe.txt

---
+++ ReadMe.txt
@@ -0,0 +1,41 @@
+Licenses and permissions for sounds:
+
+meme:
+hit_enemy_pawn: Airh Horn Sound, permitted under Creative Commons (CC-by), Autor: Gingka Akiyama
+https://lmms.io/lsp/?action=show&file=11017
+
+unfortunate_sound: 8-bit error, permitted under Creative Commons 0 Licence
+https://freesound.org/people/SamsterBirdies/sounds/363920/
+
+victory_sound: youtube Audio-Mediathek
+
+
+
+Stock audio - Free sound effects, loops and music.
+
+Licence: The drum loop is permitted for commercial use under license Creative Commons "Attribution 4.0 International Licence"
+Link to licence: https://creativecommons.org/licenses/by/4.0/legalcode
+All following sound effects are permitted under the same licence:
+
+default:
+background_music: Bossa Nova Drum With Bass And Synth Rhythm, Artist: Alexander, posted by: Alexander
+http://www.orangefreesounds.com/bossa-nova-drum-with-bass-and-synth-rhythm/
+
+hit_enemy_pawn: Pop Sound Effect, Artist: Alexander, posted by: Alexander
+http://www.orangefreesounds.com/pop-sound-effect/
+
+hit_own_pawn: Fail Sound, Artist: Alexander, posted by: Alexander
+http://www.orangefreesounds.com/fail-sound/
+
+unfortunate_sound: Wrong Answer Sound Effect, Artist: Alexander, posted by: Alexander
+http://www.orangefreesounds.com/wrong-answer-sound-effect/
+
+victory_sound: Win Fanfare Sound, Artist: Alexander, posted by: Alexander
+http://www.orangefreesounds.com/win-fanfare-sound/
+
+meme:
+background_music: Spongebob remix, Artist: /, posted by: Alexander
+http://www.orangefreesounds.com/spongebob-remix/
+
+hit_own_pawn: Roblox Death Sound, Artist: / , posted by: Alexander
+http://www.orangefreesounds.com/roblox-death-sound/
--- main.py
+++ main.py
@@ -8,55 +8,9 @@ import Help
 import Screen
 import AI
 import Victory
-import ThemePack
-'''
-meme:
-hit_enemy_pawn: Airh Horn Sound, permitted under Creative Commons (CC-by), Autor: Gingka Akiyama
-https://lmms.io/lsp/?action=show&file=11017
-
-unfortunate_sound: 8-bit error, permitted under Creative Commons 0 Licence
-https://freesound.org/people/SamsterBirdies/sounds/363920/
-
-victory_sound: youtube Audio-Mediathek
-
-Stock audio - Free sound effects, loops and music.
- 
-Licence: The drum loop is permitted for commercial use under license Creative Commons "Attribution 4.0 International Licence"
-Link to licence: https://creativecommons.org/licenses/by/4.0/legalcode
-All following sound effects are permitted under the same licence:
-
-default:
-background_music: Bossa Nova Drum With Bass And Synth Rhythm, Artist: Alexander, posted by: Alexander
-http://www.orangefreesounds.com/bossa-nova-drum-with-bass-and-synth-rhythm/
-
-hit_enemy_pawn: Pop Sound Effect, Artist: Alexander, posted by: Alexander
-http://www.orangefreesounds.com/pop-sound-effect/
-
-hit_own_pawn: Fail Sound, Artist: Alexander, posted by: Alexander
-http://www.orangefreesounds.com/fail-sound/
-
-unfortunate_sound: Wrong Answer Sound Effect, Artist: Alexander, posted by: Alexander
-http://www.orangefreesounds.com/wrong-answer-sound-effect/
-
-victory_sound: Win Fanfare Sound, Artist: Alexander, posted by: Alexander
-http://www.orangefreesounds.com/win-fanfare-sound/
-
-meme:
-background_musicmusic: Spongebob remix, Artist: /, posted by: Alexander
-http://www.orangefreesounds.com/spongebob-remix/
-
-hit_own_pawn: Roblox Death Sound, Artist: / , posted by: Alexander
-http://www.orangefreesounds.com/roblox-death-sound/
-'''
-
 
 pygame.init()
 pygame.font.init()
-pygame.mixer.init()
-
-#pygame.mixer.music.load('music/background_music.wav')
-#pygame.mixer.music.play(-1)
-#pygame.mixer.music.set_volume(.03)
 
 
 def create_players(player_count: int, ai_turn_time_delay: int, ai_difficulty: int):
@@ -73,6 +27,7 @@ my_font = pygame.font.SysFont("Arial", 50)
 text_font = pygame.font.SysFont("Arial", 30)
 start_screen_font = pygame.font.Font("Iveseenthatfacebefore.ttf", 30)
 
+# players/pawns need the gamefield to appropriately resize the pawn images
 gamefield = GameField.GameField(text_font)
 
 screen = Screen.Screen()
@@ -82,10 +37,11 @@ rules = Rules.Rules(screen, start_screen_font, "Rule.txt")
 help = Help.Help(screen, start_screen_font, "Help.txt")
 
 player_count = start_screen.start_game()
+# create new gamefield with updated theme
 gamefield = GameField.GameField(text_font)
 
 AI_TURN_TIME_DELAY = 1  # in seconds
-AI_DIFFICULTY = 1
+AI_DIFFICULTY = 1  # currently the only difficulty
 player_list = create_players(player_count, AI_TURN_TIME_DELAY, AI_DIFFICULTY)
 
 engine = Engine.Engine(player_list, gamefield, rules, help)
